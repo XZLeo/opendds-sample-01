b9f9b2e0c9ae5515077ced11ef33cab17b91d5f7
Leo Xiong <39155356+XZLeo@users.noreply.github.com>
2019-03-17 16:30:21 +0800

    Update MessageCreator.h

--- publisher/MessageCreator.h
+++ publisher/MessageCreator.h
@@ -8,7 +8,7 @@ class MessageCreator {
 public:
 
   MessageCreator(); //构造函数，无类型和返回值，名字和类名一致
-  const std::string& getRandomMessage() const;
+  const std::string& getRandomMessage() const; //const声明，表示函数不修改私有成员变量
 
 private:
 
